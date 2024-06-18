b6aa72e670d76c3bfc662ef609c1e0a264b40e70
oAbdullah <o.alabdullah@student.fontys.nl>
2024-06-18 06:59:35 -0700

    Initial commit test

---
+++ mini-game/mini-game/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  mini-game
+//
+//  Created by okb abdullah on 18/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+            Text("Okba's branch test")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mini-game/mini-game/mini_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  mini_gameApp.swift
+//  mini-game
+//
+//  Created by okb abdullah on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct mini_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
