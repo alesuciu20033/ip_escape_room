683213002512c898989d82074a27c0417b018b84
alesuciu20033 <alexandra272003@gmail.com>
2024-06-25 13:03:25 +0200

    I connect the word puzzle page with the wrong path1 page, in case the user makes any mistakes when choosing the letters.

--- logical part/logical part/Word puzzle.swift
+++ logical part/logical part/Word puzzle.swift
@@ -3,123 +3,124 @@ import CoreMotion
 
 struct WordPuzzleView: View {
     
-    // State variables to manage the words, correct letters, and current word index
     @State private var words = ["Dia?y", "H?use", "K?ife", "Wa?ch", "Phon?", "Sc?een", "?utton"]
     @State private var correctLetters = ["r", "o", "n", "t", "e", "r", "b"]
     @State private var currentWordIndex: Int? = 0
     @State private var selectedLetterIndex: Int = 0
     @State private var showPicker: Bool = false
     @State private var motionManager = CMMotionManager()
+    @State private var showCompletionButton: Bool = false
+    @State private var showErrorView: Bool = false
+    @State private var hasErrors: Bool = false
     
-    // The alphabet letters from where the player can choose
     let alphabet = Array("abcdefghijklmnopqrstuvwxyz")
     
     var body: some View {
-        ZStack {
-           
-            (Color("Background"))
-.edgesIgnoringSafeArea(.all)
-            
-            VStack {
+        NavigationStack {
+            ZStack {
+                Color("Background").edgesIgnoringSafeArea(.all)
                 
-                Text("Find the hidden word by solving this puzzle!")
-                    .font(.custom("American Typewriter", size: 46))
-                    .fontWeight(.regular)
-                    .foregroundColor(.white)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                
-                HStack {
+                VStack {
+                    Text("Find the hidden word by solving this puzzle!")
+                        .font(.custom("American Typewriter", size: 46))
+                        .fontWeight(.regular)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding()
                     
-                    Divider()
-                        .background(Color.white)
-                        .frame(width: 2)
+                    HStack {
+                        Divider()
+                            .background(Color.white)
+                            .frame(width: 2)
+                        
+                        VStack {
+                            ForEach(words.indices, id: \.self) { index in
+                                HStack {
+                                    if index == currentWordIndex {
+                                        formattedWord(words[index])
+                                            .font(.custom("American Typewriter", size: 36))
+                                            .foregroundColor(.white)
+                                            .onTapGesture {
+                                                showPicker = true
+                                            }
+                                    } else {
+                                        formattedWord(words[index])
+                                            .font(.custom("American Typewriter", size: 36))
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                                .padding(.vertical, 8)
+                            }
+                        }
+                        
+                        Divider()
+                            .background(Color.white)
+                            .frame(width: 2)
+                    }
                     
-                    VStack {
-                        // The player can go through each word in the list until he finds the one that he thinks is correct
-                        ForEach(words.indices, id: \.self) { index in
-                            HStack {
-                                // If the word is the current word being solved
-                                if index == currentWordIndex {
-                                    formattedWord(words[index])
-                                        .font(.custom("American Typewriter", size: 36))
-                                        .foregroundColor(.white)
-                                        .onTapGesture {
-                                            showPicker = true
-                                        }
-                                } else {
-                                    // If the word is not the current word being solved
-                                    formattedWord(words[index])
-                                        .font(.custom("American Typewriter", size: 36))
-                                        .foregroundColor(.white)
+                    Spacer()
+                    
+                    if showPicker {
+                        VStack {
+                            Picker("Select Letter", selection: $selectedLetterIndex) {
+                                ForEach(0..<alphabet.count, id: \.self) { index in
+                                    Text(String(alphabet[index]))
+                                        .foregroundColor(.black)
+                                        .font(.custom("American Typewriter", size: 24))
                                 }
                             }
-                            .padding(.vertical, 8)
+                            .pickerStyle(WheelPickerStyle())
+                            .background(Color.white)
+                            .frame(height: 150)
+                            .clipped()
+                            .border(Color.white, width: 2)
+                            .padding()
+                            
+                            Button("Done") {
+                                applySelectedLetter()
+                                moveToNextWord()
+                                showPicker = false
+                            }
+                            .foregroundColor(.white)
+                            .padding()
                         }
                     }
                     
-                   
-                    Divider()
-                        .background(Color.white)
-                        .frame(width: 2)
-                }
-                
-                Spacer()
-                
-                // Show the picker if showPicker is true
-                if showPicker {
-                    VStack {
-                        Picker("Select Letter", selection: $selectedLetterIndex) {
-                            ForEach(0..<alphabet.count, id: \.self) { index in
-                                Text(String(alphabet[index]))
-                                    .foregroundColor(.black)
-                                    .font(.custom("American Typewriter", size: 24))
+                    if showCompletionButton {
+                        Button("Complete") {
+                            checkForErrors()
+                            if hasErrors {
+                                showErrorView = true
                             }
-                        }
-                        .pickerStyle(WheelPickerStyle())
-                        .background(Color.white)
-                        .frame(height: 150)
-                        .clipped()
-                        .border(Color.white, width: 2)
-                        .padding()
-                        
-                        // Button to confirm letter selection
-                        Button("Done") {
-                            applySelectedLetter()
-                            moveToNextWord()
-                            showPicker = false
                         }
                         .foregroundColor(.white)
                         .padding()
+                        .background(Color("Button"))
+                        .cornerRadius(8)
                     }
                 }
             }
-        }
-        .onAppear {
-            startShakeDetection()
+            .onAppear {
+                startShakeDetection()
+            }
+            .navigationDestination(isPresented: $showErrorView) {
+                WrongPathView1()
+            }
         }
     }
     
-    // Function to format the word by highlighting the placeholder character in red
     func formattedWord(_ word: String) -> Text {
         var formatted = Text("")
-        
-        // Loop through each character in the word
         for char in word {
             if char == "?" {
-                //
                 formatted = formatted + Text(String(char)).foregroundColor(Color("Red"))
-
             } else {
-                // Otherwise, keep the character as is
                 formatted = formatted + Text(String(char))
             }
         }
-        
         return formatted
     }
 
-    // Start accelerometer to detect shake gesture
     func startShakeDetection() {
         guard motionManager.isAccelerometerAvailable else { return }
         
@@ -130,7 +131,6 @@ struct WordPuzzleView: View {
             let acceleration = data.acceleration
             let magnitude = sqrt(acceleration.x * acceleration.x + acceleration.y * acceleration.y + acceleration.z * acceleration.z)
             
-            // If shake is detected
             if magnitude > 2.5 {
                 applySelectedLetter()
                 moveToNextWord()
@@ -138,29 +138,41 @@ struct WordPuzzleView: View {
         }
     }
     
-    // This function applies the letter that was selected by the player to the current word
     func applySelectedLetter() {
         if let index = currentWordIndex {
-            let correctLetter = String(alphabet[selectedLetterIndex])
+            let selectedLetter = String(alphabet[selectedLetterIndex])
             var word = words[index]
             if let range = word.range(of: "?") {
-                word.replaceSubrange(range, with: correctLetter.uppercased())
+                word.replaceSubrange(range, with: selectedLetter.uppercased())
                 words[index] = word
             }
         }
     }
     
-    // Move to the next word in the list
     func moveToNextWord() {
         if let currentWordIndex = currentWordIndex, currentWordIndex < words.count - 1 {
             self.currentWordIndex = currentWordIndex + 1
         } else {
-            self.currentWordIndex = 0
+            self.currentWordIndex = nil
+            showCompletionButton = true
+        }
+    }
+    
+    func checkForErrors() {
+        hasErrors = false
+        for (index, word) in words.enumerated() {
+            let correctLetter = correctLetters[index]
+            if let range = word.range(of: "?") {
+                hasErrors = true
+                break
+            }
+            if !word.lowercased().contains(correctLetter) {
+                hasErrors = true
+                break
+            }
         }
     }
 }
-
-
 
 // Preview
 struct WordPuzzleView_Previews: PreviewProvider {
